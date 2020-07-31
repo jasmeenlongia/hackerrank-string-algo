a9547133ee4614a331f4c0c25b7dae0220e8826d
jasmeenlongia <66633428+jasmeenlongia@users.noreply.github.com>
2020-07-31 21:57:20 +0530

    Create String_Construction.py

---
+++ String_Construction.py
@@ -0,0 +1,19 @@
+def stringConstruction(s):
+    unique = set(s)
+    l = len(unique)
+    return l
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = stringConstruction(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
+
