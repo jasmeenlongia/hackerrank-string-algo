fc2f2df2fe916d19d201bc35d93182f73d585e14
jasmeenlongia <66633428+jasmeenlongia@users.noreply.github.com>
2020-07-31 21:56:39 +0530

    Create Separate_the_Numbers.py

---
+++ Separate_the_Numbers.py
@@ -0,0 +1,18 @@
+def sequential(s,sub_string):
+    if not s: 
+        return True
+    if s.startswith(sub_string):
+        l = len(sub_string)
+        return sequential(s[l:],str(int(sub_string)+1))
+    return False
+    
+def separateNumbers(s):
+    for i in range(1,len(s)//2+1):
+        sub_string = s[:i]
+        if sequential(s,sub_string):
+            return "YES "+sub_string
+    return "NO"
+
+for _ in range(int(input())):
+    s = input()
+    print(separateNumbers(s))
